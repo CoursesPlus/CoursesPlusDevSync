d7202ab23fa267d08838f21b58cd5c385d0e8da3
Alex Studer <alex_studer@outlook.com>
2015-02-12 22:09:04 -0500

    Began work on sync program

--- src/tk/coursesplus/development/devsync/CoursesPlusDevSync.java
+++ src/tk/coursesplus/development/devsync/CoursesPlusDevSync.java
@@ -5,11 +5,19 @@ import java.awt.Font;
 import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.FileSystems;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.StandardWatchEventKinds;
+import java.nio.file.WatchEvent;
+import java.nio.file.WatchKey;
+import java.nio.file.WatchService;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
-import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JTextArea;
@@ -23,7 +31,9 @@ public class CoursesPlusDevSync extends JFrame implements ActionListener {
 	JLabel title = new JLabel("Courses+ Dev Sync", JLabel.CENTER);
 	JLabel instructions = new JLabel("<html><center>This program syncs the different folders in the Courses+ directory with each other. This is required for cross-browser development.</center></html>", JLabel.CENTER);
 	JButton startBtn = new JButton("Start!");
-	JTextArea log = new JTextArea();
+	public static JTextArea log = new JTextArea();
+	public static WatchService watcher;
+	public static Path sourceCodePath;
 	JScrollPane scroll;
 	
 	public CoursesPlusDevSync() {
@@ -73,22 +83,93 @@ public class CoursesPlusDevSync extends JFrame implements ActionListener {
 	}
 	
 	public void actionPerformed(ActionEvent event)
-	  {
+	{
 	    Object source = event.getSource();
 	    if (source == startBtn)
 	    {
 		    addLogEntry("Starting sync...");
-		    JOptionPane.showMessageDialog(null,"Hello!","Important message",
-		    JOptionPane.PLAIN_MESSAGE); setVisible(true);  // show something
+		    //JOptionPane.showMessageDialog(null,"Hello!","Important message",
+		    //JOptionPane.PLAIN_MESSAGE); setVisible(true);  // show something
+		    try {
+			    String pathPlace = System.getProperty("user.home") + "/Documents/Git/CoursesPlus/CoursesPlus/";
+			    File fileThing = new File(pathPlace);
+			    sourceCodePath = fileThing.toPath();
+			    addLogEntry("Found user's home directory: " + System.getProperty("user.home"));
+			    addLogEntry("Found source code directory: " + sourceCodePath.toString());
+			    if (!fileThing.isDirectory() || !fileThing.exists()) {
+			    	addLogEntry("[ERROR] Source code directory is not a directory or doesn't exist!");
+			    	return;
+			    }
+				watcher = FileSystems.getDefault().newWatchService();
+				WatchKey key = sourceCodePath.register(watcher, StandardWatchEventKinds.ENTRY_CREATE, StandardWatchEventKinds.ENTRY_DELETE, StandardWatchEventKinds.ENTRY_MODIFY);
+				SyncThread thread = new SyncThread();
+				thread.start();
+			} catch (IOException e) {
+				addLogEntry("[ERROR] IOException during setup!");
+				e.printStackTrace();
+			}
 	    }
 	  }
 	
-	public void addLogEntry(String str) {
-		this.log.append("[LOG] " + str + "\n");
+	public static void addLogEntry(String str) {
+		log.append("[LOG] " + str + "\n");
 	}
 	
 	public static void main(String[] args) {
 		new CoursesPlusDevSync();
 	}
 
+}
+
+class SyncThread extends Thread {
+	public void run()                       
+    {            
+		while (true) {
+			// wait for key to be signaled
+		    WatchKey key;
+		    key = CoursesPlusDevSync.watcher.poll();
+		    
+		    if (key == null) {
+		    	continue;
+		    }
+		    
+		    for (WatchEvent<?> event: key.pollEvents()) {
+		        WatchEvent.Kind<?> kind = event.kind();
+
+		        // This key is registered only
+		        // for ENTRY_CREATE events,
+		        // but an OVERFLOW event can
+		        // occur regardless if events
+		        // are lost or discarded.
+		        if (kind == StandardWatchEventKinds.OVERFLOW) {
+			    	CoursesPlusDevSync.addLogEntry("Nope, overflow.");
+		            continue;
+		        }
+		        
+		        // The filename is the
+		        // context of the event.
+		        @SuppressWarnings("unchecked")
+				WatchEvent<Path> ev = (WatchEvent<Path>)event;
+		        Path filename = ev.context();
+		        
+		        // Resolve the filename against the directory.
+	            Path child = CoursesPlusDevSync.sourceCodePath.resolve(filename);
+	            CoursesPlusDevSync.addLogEntry("File " + filename + " has changed!");
+		    }
+
+		    // Reset the key -- this step is critical if you want to
+		    // receive further watch events.  If the key is no longer valid,
+		    // the directory is inaccessible so exit the loop.
+		    boolean valid = key.reset();
+		    if (!valid) {
+		        break;
+		    }
+
+		    try {
+				SyncThread.sleep(1);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
+    }
 }
